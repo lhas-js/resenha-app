b269a370500882e3bc88b26e59a4aa603143f1fa
lhas <luizhrqas@gmail.com>
2019-06-27 13:28:11 -0300

    Funcionalidade finalizada

--- src/routes/Chat.js
+++ src/routes/Chat.js
@@ -1,23 +1,82 @@
 import firebase from "firebase";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Badge, Card, ListGroup } from "react-bootstrap";
+import TimeAgo from "react-timeago";
 
 const Chat = props => {
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const messageListRef = useRef(null);
+
   useEffect(() => {
     const db = firebase.firestore();
     db.collection(`rooms/${props.match.params.room}/messages`)
-      .get()
-      .then(snapshot => {
+      .orderBy("createdAt")
+      .onSnapshot(snapshot => {
         const response = snapshot.docs.map(snapshot => ({
           id: snapshot.id,
           ...snapshot.data()
         }));
         setMessages(response);
+        messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
       });
-  }, []);
+  }, [props]);
 
-  console.log("props", props);
+  const onSubmit = event => {
+    event.preventDefault();
 
-  return <div>ae</div>;
+    const db = firebase.firestore();
+    db.collection(`rooms/${props.match.params.room}/messages`).add({
+      createdAt: +new Date(),
+      username: props.match.params.name,
+      content: message
+    });
+    setMessage("");
+  };
+
+  return (
+    <div style={{ margin: "1vh" }}>
+      <Card
+        ref={messageListRef}
+        style={{ height: "calc(98vh - 100px)", overflowY: "scroll" }}
+      >
+        <ListGroup>
+          {messages.map(_message => (
+            <ListGroup.Item key={_message.id}>
+              <strong>@{_message.username}</strong>
+              <time
+                style={{ fontSize: 12, marginLeft: 5, color: "var(--gray)" }}
+              >
+                <TimeAgo date={_message.createdAt} />
+              </time>
+              <p>{_message.content}</p>
+              <Badge pill variant="light" />
+            </ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Card>
+
+      <Card style={{ height: "100px" }}>
+        <form
+          onSubmit={onSubmit}
+          style={{
+            display: "flex",
+            height: "100%",
+            alignItems: "center",
+            margin: "20px"
+          }}
+        >
+          <input
+            value={message}
+            onChange={event => setMessage(event.target.value)}
+            style={{ width: "100%" }}
+            type="text"
+          />
+          <button>OK</button>
+        </form>
+      </Card>
+    </div>
+  );
 };
+
 export default Chat;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import Chat from "./Chat";
 import Home from "./Home";
 
 const Routes = () => (
-  <Router>
+  <BrowserRouter>
     <div>
       <Route path="/" exact component={Home} />
-      <Route path="/chat/:name/:room" component={Chat} />​
+      <Route path="/chat/:name/:room" component={Chat} />
     </div>
-  </Router>
+  </BrowserRouter>
 );
 
 export default Routes;
